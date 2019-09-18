03f5eaf0d866be0ea0d3b81b5ed6ec4f1cce4233
Hanrell Ramon <hanrellsouza@gmail.com>
2019-09-17 23:09:25 -0300

    Add files via upload

---
+++ Chat/Cliente/src/com/hanrell/app/Cliente.java
@@ -0,0 +1,33 @@
+package com.hanrell.app;
+
+import com.hanrell.app.frame.ClienteFrame;
+
+public class Cliente {
+
+    
+    public static void main(String args[]) {
+        
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(ClienteFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(ClienteFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(ClienteFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(ClienteFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new ClienteFrame().setVisible(true);
+            }
+        });
+    }
+}
---
+++ Chat/Servidor/src/com/hanrell/app/Servidor.java
@@ -0,0 +1,9 @@
+package com.hanrell.app;
+
+import com.hanrell.app.service.ServidorService;
+
+public class Servidor {
+public static void main(String[] args) {
+        new ServidorService();
+    }
+}
