ed88df81f13f91d99a11c35d89ee836d26ad6dfa
Matheus Ramalho Arnaud <50004296+matheusarnaud@users.noreply.github.com>
2021-09-01 18:57:07 -0300

    Game (#2)
    * Starting branch
    
    * Starting branch
    
    * Removendo arquivos
    
    * Add files via upload
    
    * Add files via upload
    
    * Refatorando codigo e pacotes, iniciando menu GUI
    
    * Add files via upload
    
    Co-authored-by: pedrohcg <52712904+pedrohcg@users.noreply.github.com>

---
+++ JogoXadrez/Peao.java
@@ -0,0 +1,106 @@
+
+/**
+ * Write a description of class Peao here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Peao extends Peca
+{
+    private boolean enPassant;
+    /**
+     * Constructor for objects of class Peao
+     */
+    public Peao(Casa casa, int tipo) 
+    {
+        super(casa, tipo);
+        enPassant = false;
+    }
+
+    public void mover(Casa destino, Jogo jogo)
+    {
+        int xf = destino.getX();
+        int yf = destino.getY();
+        int xi = casa.getX();
+        int yi = casa.getY();
+        Tabuleiro tabuleiro = jogo.getTabuleiro();
+        
+        //movimento normal
+        if (xf == xi) {
+            if (!destino.possuiPeca() && !temPecaPerto(casa, destino, tabuleiro)) {
+                //Movimento do peao branco.
+                if (tipo == PEAO_BRANCO && yf == yi + 1) {
+                    irPara(destino);
+                    enPassant = false;
+                }
+                //Movimento do peao preto.
+                else if (tipo == PEAO_PRETO && yf == yi - 1) {
+                    irPara(destino);
+                    enPassant = false;
+                }
+                //Andar 2 casas com o peao branco quando ele está na sua casa inicial.
+                else if (tipo == PEAO_BRANCO && yi == 1 && yf == yi + 2) {
+                    irPara(destino);
+                    enPassant = true;
+                }
+                //Andar 2 casas com o peao branco quando ele está na sua casa inicial.
+                else if (tipo == PEAO_PRETO && yi == 6 && yf == yi - 2) {
+                    irPara(destino);
+                    enPassant = true;
+                }
+            }
+        }
+        //movimento de captura
+        if (Math.abs(xf - xi) == 1) {
+            capturar(destino, jogo);
+        }
+    }
+    
+    public void capturar(Casa destino, Jogo jogo) 
+    {
+        int xf = destino.getX();
+        int yf = destino.getY();
+        int xi = casa.getX();
+        int yi = casa.getY();
+        Tabuleiro tabuleiro = jogo.getTabuleiro();
+        
+        //captura normal
+        if (destino.possuiPeca()) {
+            //Captura com peao branco.
+            if (tipo == PEAO_BRANCO && yf == yi + 1) {
+                irPara(destino);
+                enPassant = false;
+            }
+            //Captura com peao preto.
+            else if (tipo == PEAO_PRETO && yf == yi - 1) {
+                irPara(destino);
+                enPassant = false;
+            }
+        } else { //captura en-passant
+            Peao peaoAnterior = null;
+            if (jogo.pecaAnterior.tipo == PEAO_BRANCO || jogo.pecaAnterior.tipo == PEAO_PRETO) {
+                peaoAnterior = (Peao) jogo.pecaAnterior;
+            }
+            //checa se o movimento é uma das 4 opções de captura en-passant válidas
+            for (int i = 1; i > -2; i = i-2) {
+                int tipoAtual = PEAO_BRANCO;
+                for (int j = 1; j > -2; j = j-2) {
+                    //checa a casa à direita (+1, 0) e à esquerda (-1, 0) do peao clicado
+                    if (peaoAnterior != null && tabuleiro.existeCasa(this.casa, +i, 0)) {
+                        if (peaoAnterior.enPassant == true && tabuleiro.getCasa(this.casa, +i, 0).possuiPeca()) {
+                            Peca that = tabuleiro.getCasa(this.casa, +i, 0).getPeca();
+                            if (that == peaoAnterior && this.tipo == tipoAtual && this.coresDiferentes(that)) {
+                                if (xf == xi + i && yf == yi + j) {
+                                    this.irPara(destino);
+                                    this.enPassant = false;
+                                    that.casa.removerPeca();
+                                }
+                            }
+                        }
+                    }
+                    tipoAtual = PEAO_PRETO;
+                }
+            }
+        }
+    }
+}
---
+++ src/com/game/Tabuleiro.java
@@ -0,0 +1,78 @@
+package com.game;
+
+/**
+ * O Tabuleiro do jogo. 
+ * Respons�vel por armazenar as 64 casas.
+ * 
+ * @author Alan Moraes &lt;alan@ci.ufpb.br&gt;
+ * @author Leonardo Villeth &lt;lvilleth@cc.ci.ufpb.br&gt;
+ */
+public class Tabuleiro {
+
+    private Casa[][] casas;
+
+    public Tabuleiro()
+    {
+        casas = new Casa[8][8];
+        for (int x = 0; x < 8; x++) {
+            for (int y = 0; y < 8; y++) {
+                Casa casa = new Casa(x, y);
+                casas[x][y] = casa;
+            }
+        }
+    }
+    
+    /**
+     * @param x linha
+     * @param y coluna
+     * @return Casa na posicao (x,y)
+     */
+    public Casa getCasa(int x, int y)
+    {
+        if((x <= 7) && (x >= 0) && (y <= 7) && (y >= 0))
+        {
+            return casas[x][y];
+        }
+        else
+        {
+            return null;
+        }
+    }
+    
+    public Casa getCasa(Casa casaAtual, int i, int j)
+    {
+        int x = casaAtual.getX();
+        int y = casaAtual.getY();
+        
+        return casas[x + i][y + j];
+    }
+    
+    /**
+     * @param x linha
+     * @param y coluna
+     * @return true se existe casa na coordenada (x, y), caso contrario false.
+     */
+    public boolean existeCasa(int x, int y){
+        if (x >= 0  && x <= 7 && y >=0 && y <= 7) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    /**
+     * @param x linha
+     * @param y coluna
+     * @return true se existe casa na coordenada (x, y), caso contrario false.
+     */
+    public boolean existeCasa(Casa casaAtual, int i, int j){
+        int x = casaAtual.getX() + i;
+        int y = casaAtual.getY() + j;
+        
+        if (x >= 0  && x <= 7 && y >=0 && y <= 7) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
